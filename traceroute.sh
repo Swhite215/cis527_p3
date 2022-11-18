dbd03d981e33f1a6d51ba4521440bd37f68c9ad4
Spencer White <swhite215@live.com>
2022-11-18 12:55:10 -0500

    eight possible sites to work with

--- traceroute.sh
+++ traceroute.sh
@@ -3,7 +3,14 @@
 # traceroute www.google.com >> tr_logs.txt
 
 # Mapping of IP to Domain
-one="www.ford.com: 23.61.127.8"
+one="www.ford.com: 23.61.127.8" # Ford
+two="www.google.com: 142.250.191.174" # Google
+three="www.amazon.com: 18.160.199.67" # Amazon
+four="www.ford.com.cn: 40.73.122.39" # Ford China
+five="www.imma.ie: 172.67.164.21" # Irish Art Museum
+six="nemox.net: 83.137.41.11" #Austria
+seven="registro.br: 200.160.2.3" # Brazil
+eight="network-tools.com: 141.193.213.11" # Canada
 
 # Array of IP Addresses
 arr=("www.ford.com")
